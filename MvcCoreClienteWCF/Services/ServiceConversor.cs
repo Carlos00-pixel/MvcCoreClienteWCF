ef691e2db27836faf4247129e8e8fb4a5787fb88
Carlos00-pixel <carlos.mayoral00@gmail.com>
2023-03-23 14:25:28 +0100

    Commit

--- MvcCoreClienteWCF/Services/ServiceConversor.cs
+++ MvcCoreClienteWCF/Services/ServiceConversor.cs
@@ -1,6 +1,16 @@
-﻿namespace MvcCoreClienteWCF.Services
+﻿using ServiceReferenceConversor;
+
+namespace MvcCoreClienteWCF.Services
 {
     public class ServiceConversor
     {
+        NumberConversionSoapTypeClient client;
+
+        public ServiceConversor()
+        {
+            this.client = 
+                new NumberConversionSoapTypeClient
+                (NumberConversionSoapTypeClient.EndpointConfiguration.NumberConversionSoap);
+        }
     }
 }
