b7e0eb26008f89f1978a21f5fdd498c85aea024e
Carlos00-pixel <carlos.mayoral00@gmail.com>
2023-03-23 14:14:46 +0100

    Add project files.

---
+++ MvcCoreClienteWCF/Services/ServicesCountries.cs
@@ -0,0 +1,32 @@
+﻿using ServiceCountries;
+
+namespace MvcCoreClienteWCF.Services
+{
+    public class ServicesCountries
+    {
+        CountryInfoServiceSoapTypeClient client;
+
+        public ServicesCountries()
+        {
+            this.client =
+                new CountryInfoServiceSoapTypeClient
+                (CountryInfoServiceSoapTypeClient.EndpointConfiguration.CountryInfoServiceSoap);
+        }
+        public async Task<tCountryCodeAndName[]> GetCountriesAsync()
+        {
+            ListOfCountryNamesByNameResponse response =
+                await this.client.ListOfCountryNamesByNameAsync();
+            tCountryCodeAndName[] datos =
+                response.Body.ListOfCountryNamesByNameResult;
+            return datos;
+        }
+
+        public async Task<tCountryInfo> GetCountryInfoAsync(string isoCode)
+        {
+            FullCountryInfoResponse response =
+                await this.client.FullCountryInfoAsync(isoCode);
+            tCountryInfo country = response.Body.FullCountryInfoResult;
+            return country;
+        }
+    }
+}
